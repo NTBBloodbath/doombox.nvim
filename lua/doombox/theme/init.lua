79331097b52f7a8fea60d15587ef64ca5d459fd3
NTBBloodbath <bloodbathalchemist@protonmail.com>
2021-10-29 10:00:49 -0400

    feat: adjust Pmenu selection

--- lua/doombox/theme/init.lua
+++ lua/doombox/theme/init.lua
@@ -70,8 +70,8 @@ theme.setup = function(config)
     StatusLinePartNC = { fg = cb.fg_alt, bg = cb.bg_popup, style = "bold" },
 
     Pmenu = { fg = cb.fg, bg = cb.bg_highlight },
-    PmenuSel = { fg = cb.grey, bg = cb.blue },
-    PmenuSelBold = { fg = cb.grey, bg = cb.blue, style = "bold" },
+    PmenuSel = { fg = cb.fg_alt, bg = cb.bg_popup },
+    PmenuSelBold = { fg = cb.fg_alt, bg = cb.bg_popup, style = "bold" },
     PmenuSbar = { bg = cb.bg_alt },
     PmenuThumb = { bg = cb.fg },
 
